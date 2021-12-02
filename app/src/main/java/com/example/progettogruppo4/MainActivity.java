da10f7a18296c7b009f882d48f628efe0c4aea10
alyno18 <alybonomi18@gmail.com>
2021-12-02 11:37:25 +0100

    bonomi

--- app/src/main/java/com/example/progettogruppo4/MainActivity.java
+++ app/src/main/java/com/example/progettogruppo4/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.progettogruppo4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    button change;
+    Textview nome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nome = (Textview) findViewById(nome);
+        change = (button) findViewById(change);
+    }
+
+    public void changeText(){
+
     }
 }
