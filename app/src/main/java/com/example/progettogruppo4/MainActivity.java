3c228c073f006ba27e437d93e9af1d093a0ba937
alyno18 <alybonomi18@gmail.com>
2021-12-02 11:43:36 +0100

    bonomi situazione 2

--- app/src/main/java/com/example/progettogruppo4/MainActivity.java
+++ app/src/main/java/com/example/progettogruppo4/MainActivity.java
@@ -2,26 +2,25 @@ package com.example.progettogruppo4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    button change;
-    Textview nome;
+    Button change;
+    TextView nome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        nome = (Textview) findViewById(nome);
-        change = (button) findViewById(change);
+        nome = (TextView) findViewById(R.id.nome);
+        change = (Button) findViewById(R.id.change);
     }
 
     public void changeText(){
-
+        nome.setText("Bonomi");
+        setContentView(nome);
     }
 }
